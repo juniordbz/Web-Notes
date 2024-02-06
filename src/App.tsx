4495e094da092abf7612e05efa88ac828c09c40c
Francisco Bezerra da Silva Junior <franciscojunior02@hotmail.com.br>
2024-02-06 19:53:49 -0300

    cards and modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import logo from './assets/logo.svg'
+import { NoteCard } from './components/note-card'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const note = {
+  createdDate: new Date(),
+  content: 'Hello world',
 }
 
-export default App
+export function App() {
+  return (
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <img src={logo} alt="logo NLW Expert" />
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Busque em usas notas..."
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500"
+        />
+      </form>
+
+      <div className="border-slate-700 border" />
+
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <div className="rounded-md bg-slate-700 p-5 space-y-3">
+          <span className="font-medium text-slate-200 text-sm">
+            Adicionar nota
+          </span>
+          <p className="text-sm leading-6 text-slate-400">
+            Grave uma nota em áudio que será convertida para texto
+            automaticamente.
+          </p>
+        </div>
+
+        <NoteCard note={note} />
+      </div>
+    </div>
+  )
+}
