90cc9913c66240792f2d8def5e4ff6d5320f09a6
Francisco Bezerra da Silva Junior <franciscojunior02@hotmail.com.br>
2024-02-06 20:00:27 -0300

    component New Note Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import logo from './assets/logo.svg'
+import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
 const note = {
@@ -21,16 +22,7 @@ export function App() {
       <div className="border-slate-700 border" />
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <div className="rounded-md bg-slate-700 p-5 space-y-3">
-          <span className="font-medium text-slate-200 text-sm">
-            Adicionar nota
-          </span>
-          <p className="text-sm leading-6 text-slate-400">
-            Grave uma nota em áudio que será convertida para texto
-            automaticamente.
-          </p>
-        </div>
-
+        <NewNoteCard />
         <NoteCard note={note} />
       </div>
     </div>
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -15,10 +15,12 @@ export function NoteCard({ note }: NoteCardProps) {
     <Dialog.Root>
       <Dialog.Trigger className="flex flex-col rounded-md text-left bg-slate-800 p-5 gap-3 overflow-hidden relative hover:ring-2 hover: ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
         <span className="font-medium text-slate-300 text-sm">
-          {note.createdDate.toISOString()}
+          {formatDistanceToNow(note.createdDate, {
+            locale: ptBR,
+            addSuffix: true,
+          })}
         </span>
         <p className="text-sm leading-6 text-slate-400">{note.content}</p>
-        <div className="absolute bottom-0 right-0 left-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
       </Dialog.Trigger>
 
       <Dialog.Portal>
