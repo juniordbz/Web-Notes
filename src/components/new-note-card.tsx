5e5a7aedec6d68e358730078c873c7449ab14de1
Francisco Bezerra da Silva Junior <franciscojunior02@hotmail.com.br>
2024-02-07 21:54:47 -0300

    responsiveness and delete note

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -101,7 +101,7 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
 
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 fixed bg-black/50" />
-        <Dialog.Content className="fixed rounded-md overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 flex flex-col outline-none">
+        <Dialog.Content className="fixed md:rounded-md overflow-hidden inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] md:w-full md:h-[60vh] bg-slate-700 flex flex-col outline-none">
           <Dialog.Close
             onClick={handleModalChange}
             className="absolute right-0 top-0 p-1.5 bg-slate-800 text-slate-400 hover:text-slate-100"
