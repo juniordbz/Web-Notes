d37cf5652a75b769e3e06b710bb21851665460a6
Francisco Bezerra da Silva Junior <franciscojunior02@hotmail.com.br>
2024-02-07 22:05:11 -0300

    typo fix

--- src/App.tsx
+++ src/App.tsx
@@ -61,7 +61,7 @@ export function App() {
       <form className="w-full">
         <input
           type="text"
-          placeholder="Busque em usas notas..."
+          placeholder="Busque em suas notas..."
           className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500"
           onChange={handleSearch}
         />
